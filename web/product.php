e033c082de5a19e61b4a187a84ab61cdc1101dce
{anna-mukomela} <annam@default-value.com>
2021-10-05 16:29:39 +0300

    #2 Shop Skeleton: added data.php for categories and products. added data.php for categories and products; implemented top menu

--- web/product.php
+++ web/product.php
@@ -1,3 +1,6 @@
+<?php
+    require_once 'data.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -22,35 +25,38 @@
     <a href="/" title="{DV.Campus} PHP Framework">
         <img src="logo.jpg" alt="{DV.Campus} Logo" width="200"/>
     </a>
-    <menu>
-        <li>Category 1</li>
-        <li>Category 2</li>
-        <li>Category 3</li>
-        <li>Category 4</li>
-    </menu>
-</header>
-<main>
-    <img src="product-placeholder.png" alt="Product 1" width="300"/>
-    <h1>Product 1</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi cumque dolore eos exercitationem, impedit laudantium minima minus molestias quaerat quam, quidem repellendus sit temporibus veniam veritatis.</p>
-    <span>$33.33</span>
-    <button type="button">Add To Cart</button>
-</main>
-<footer>
     <nav>
         <ul>
+            <?php foreach (catalogGetCategory() as $category) : ?>
             <li>
-                <a href="/about-us">About Us</a>
+                <a href="/<?= $category['url'] ?>"><?= $category['name'] ?><a/>
             </li>
-            <li>
-                <a href="/terms-and-conditions">Terms & Conditions</a>
-            </li>
-            <li>
-                <a href="/contact-us">Contact Us</a>
-            </li>
+            <?php endforeach; ?>
         </ul>
     </nav>
-    <p>© Default Value 2021. All Rights Reserved.</p>
-</footer>
+</header>
+    <main>
+       <img src="product-placeholder.png" alt="Product 1" width="300"/>
+        <h1>Product 1</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi cumque dolore eos exercitationem, impedit laudantium minima minus molestias quaerat quam, quidem repellendus sit temporibus veniam veritatis.</p>
+        <span>$33.33</span>
+        <button type="button">Add To Cart</button>
+    </main>
+    <footer>
+        <nav>
+            <ul>
+                <li>
+                    <a href="/about-us">About Us</a>
+                </li>
+                <li>
+                    <a href="/terms-and-conditions">Terms & Conditions</a>
+                </li>
+                <li>
+                    <a href="/contact-us">Contact Us</a>
+                </li>
+            </ul>
+        </nav>
+        <p>© Default Value 2021. All Rights Reserved.</p>
+    </footer>
 </body>
 </html>
